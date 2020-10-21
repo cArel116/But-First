8f833ea41565e10adf54c78a868edba9fcc9a48c
Colin Arel <romans_one16@live.com>
2020-10-21 11:37:25 -0500

    login/register/home pages displaying on web page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Home = () => (
+    <>
+        <h2>But First...</h2>
+
+        <div>
+            {/* QUOTE */}
+        </div>
+
+        <div>
+            {/* WEATHER */}
+        </div>
+    </>
+)
---
+++ src/components/auth/Login.js
@@ -0,0 +1,64 @@
+import React, { useRef } from "react"
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom"
+import "./Login.css"
+
+
+export const Login = props => {
+    const email = useRef()
+    const existDialog = useRef()
+    const history = useHistory()
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8088/users?email=${email.current.value}`)
+            .then(res => res.json())
+            .then(user => user.length ? user[0] : false)
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists) {
+                    localStorage.setItem("butFirst_user", exists.id)
+                    history.push("/")
+                } else {
+                    existDialog.current.showModal()
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <dialog className="dialog dialog--auth" ref={existDialog}>
+                <div>User does not exist!</div>
+                <button className="button--close" onClick={e => existDialog.current.close()}>Close</button>
+            </dialog>
+
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>But First...</h1>
+                    <h2>Please Sign In</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email Address </label>
+                        <input ref={email} type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <button type="submit">
+                            Sign In
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not registered yet?</Link>
+            </section>
+        </main>
+    )
+}
+
