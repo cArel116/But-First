740fa72cc551a281c79252abe4bdf96dc6b1af4b
Colin Arel <romans_one16@live.com>
2020-10-26 15:23:14 -0500

    Home and Weather page layouts/display @MVP+git add . Moving on to Settings and Quote provider

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -22,17 +22,17 @@ export const WeatherApp = () => {
         }
     }
 
-    const dateBuilder = (d) => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // const dateBuilder = (d) => {
+    //     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    //     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
+    //     let day = days[d.getDay()];
+    //     let date = d.getDate();
+    //     let month = months[d.getMonth()];
+    //     let year = d.getFullYear();
 
-        return `${day} ${date} ${month} ${year}`
-    }
+    //     return `${day} ${date} ${month} ${year}`
+    // }
 
 
     return (
@@ -53,7 +53,7 @@ export const WeatherApp = () => {
                     <div>
                         <div className="location-box">
                             <div className="location">{weather.name}, {weather.sys.country}</div>
-                            <div className="date">{dateBuilder(new Date())}</div>
+                            {/* <div className="date">{dateBuilder(new Date())}</div> */}
                         </div>
                         <div className="weather-box">
                             <div className="temp">
