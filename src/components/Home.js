fccedf742f1eb6a3a0aebefbd8fae28135a15efe
Colin Arel <romans_one16@live.com>
2020-10-27 11:30:16 -0500

    minor changes to css. added background img to Login screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import "./Home.css"
 
 export const Home = () => {
 
+
     const dateBuilder = (d) => {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
