435539e4b315a6f219b8a04701d3023199d488a0
Colin Arel <romans_one16@live.com>
2020-10-29 18:35:26 -0500

    LET'S GOOOOOOO! Coffee page working WITH modals

--- src/components/coffee/CoffeeCard.js
+++ src/components/coffee/CoffeeCard.js
@@ -2,17 +2,10 @@ import React, { useState } from "react"
 import "./Coffee.css"
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 
 
 export const CoffeeCard = ({ coffee }) => {
-    // <section className="coffee">
-    //     <h3 className="coffee__name">
-    //         <button to={`/coffee/${coffee.id}`}>
-    //             {coffee.name}
-    //         </button>
-    //     </h3>
-    //     <div className="coffee__breed">{coffee.breed}</div>
-    // </section >
 
     const [show, setShow] = useState(false);
 
@@ -20,10 +13,9 @@ export const CoffeeCard = ({ coffee }) => {
     const handleShow = () => setShow(true);
 
     return (
-        <div className="brewSelectionArea">
-
+        <>
             <Button to={`/coffee/${coffee.id}`} className="brewMethod" variant="primary" onClick={handleShow}>
-                <img src={coffee.img} alt={coffee.name} />
+                <Image src={coffee.img} alt={coffee.name} />
             </Button>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
@@ -41,7 +33,7 @@ export const CoffeeCard = ({ coffee }) => {
                 </Modal.Footer>
             </Modal>
 
-        </div>
+        </>
     )
 
 }
