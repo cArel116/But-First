80187477bd94eb3e18321c6bdd9704a0c0f6abb2
Colin Arel <romans_one16@live.com>
2020-11-03 17:42:21 -0600

    still trying to sort this weather thing out.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom"
 import { Nav } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,7 +10,13 @@ import FetchQuote from "./quote/QuoteProvider";
 
 export const Home = () => {
 
+    const [weather, setWeather] = useState({});
+
     const quote = FetchQuote();
+    const city = `http://localhost:8088/userSettings/settings/localWeatherSettings/${city}`
+    const state = `http://localhost:8088/userSettings/settings/localWeatherSettings/${state}`
+    const country = `http://localhost:8088/userSettings/settings/localWeatherSettings/${country}`
+    const zip = `http://localhost:8088/userSettings/settings/localWeatherSettings/${zip}`
 
     const dateBuilder = (d) => {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -50,7 +56,11 @@ export const Home = () => {
 
                     <div className="date">{dateBuilder(new Date())}</div>
                     <div className="localTemp--homeDiv">
-                        <h2 className="localTemp--home">48°F</h2>
+                        <h2 className="localTemp--home" onLoad={() =>
+                            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&${state}&${country}||zip=${zip})&appid=be83c66a552cdf75a46cb29c6e903fd5`) //**IF YOU WANT FORECAST, IT GOES IN THE WEATHER?Q= LINE TO THE LEFT**
+                                .then(res => res.json())
+                                .then(setWeather)}>
+                            {Math.round(weather.main.temp)}°F</h2>
                     </div>
 
                     <div className="dailyQuote--homeDiv">
